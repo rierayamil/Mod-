324d855cf60913d6b96d620a07e0f481436f929a
yamil <rierayamil@gmail.com>
2023-03-29 04:01:56 -0300

    correcion de diseño y agregado de cards y carrousel infinito

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+});
+ScrollReveal().reveal('.card',{delay:500, origin:'left'});
+ScrollReveal().reveal('.card2',{delay:500, origin:'right'});
+
