dd88cda27bb58a79c1ec4816c4a7cc2d1c69cbfe
yamil <rierayamil@gmail.com>
2023-03-31 05:59:54 -0300

    se agrego tarjeta info en index

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 
 ScrollReveal({
-  reset: true,
-  distance: '60px',
+  reset: false,
+  distance: '50px',
   duration: 2500,
   delay: 300
 });
 ScrollReveal().reveal('.card-first',{delay:500, origin:'left'});
 ScrollReveal().reveal('.card-second',{delay:500, origin:'right'});
 ScrollReveal().reveal('.container-title',{delay:500, origin:'right'});
+ScrollReveal().reveal('.main-title',{delay:500, origin:'bottom'});
+ScrollReveal().reveal('.content',{delay:500, origin:'right'});
+ScrollReveal().reveal('.container ,.text-box',{delay:500, origin:'bottom'});
